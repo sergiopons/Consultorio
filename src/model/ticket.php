8c05c79dee395cdfc11d2345c0484ff62ad883cf
Sergio <serpons@gmail.com>
2020-11-03 12:23:58 +0100

    commit sergio

--- src/model/ticket.php
+++ src/model/ticket.php
@@ -12,15 +12,66 @@ class ticket {
   private $database;
   private $table = "agenda";
 
-public function __construct(int $id = null, string $coderTeam ='', string $topic = '', string $dateTime = null, string $description = '') {
-  $this->id = $id;
-  $this->coderTeam = $coderTeam;
-  $this->topic = $topic;
-  $this->dateTime = $dateTime;
-  $this->description = $description;
+  public function __construct(int $id = null, string $coderTeam ='', string $topic = '', string $dateTime = null, string $description = '') {
+    $this->id = $id;
+    $this->coderTeam = $coderTeam;
+    $this->topic = $topic;
+    $this->dateTime = $dateTime;
+    $this->description = $description;
 
-  if (!$this->database) {
-    $this->database = new database();
+    if (!$this->database) {
+      $this->database = new database();
+      }
     }
+  
+  public function getCoderTeam()
+  {
+    return $this->coderTeam;
+  }  
+
+  public function getId()
+  {
+    return $this->id;
   }
+
+  public function getTopic()
+  {
+    return $this->topic;
+  }
+
+  public function getDateTime()
+    {
+        return $this->dateTime;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function renameCoderTeam($coderTeam)
+    {
+        $this->coderTeam = $coderTeam;
+    }
+
+    public function changeTopic($topic)
+    {
+        $this->topic = $topic;
+    }
+
+    public function changeDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function save(): void
+    {
+        $this->database->mysql->query("INSERT INTO `{$this->table}` (`coderTeam`) VALUES ('$this->coderTeam');");
+    }
+
+
+
+  
+
 }
+
