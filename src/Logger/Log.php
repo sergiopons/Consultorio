10564a7a3a749f8f4cf9326c855fbe11bd68885f
renejfc <renejfc@gmail.com>
2020-11-12 14:34:11 +0100

    changes

--- src/Logger/Log.php
+++ src/Logger/Log.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Logger;
+
 use  App\Ticket;
 
-class Log 
+class Log
 {
 
-    public function __construct(string $action = '', string $message = '', int $id = null) 
+    public function __construct(string $action = '', string $message = '', int $id = null)
     {
         $this->time = date("Y-m-d H:i:s");
         $this->action =  $action;
@@ -16,14 +17,16 @@ class Log
 
     public function logInFile(): void
     {
-        $data = array('Time' => $this->time,
-                     'Action' => $this->action, 
-                     'Message' => $this->message,
-                     'id' =>$this->id); 
+        $data = [
+            'Time' => $this->time,
+            'Action' => $this->action,
+            'Message' => $this->message,
+            'id' => $this->id
+        ];
 
-        $json_string = json_encode($data, JSON_PRETTY_PRINT);            
+        $json_string = json_encode($data, JSON_PRETTY_PRINT);
         $fileLog = fopen("src/Logger/Log.json", "a");
-        fwrite($fileLog,$json_string . "\n");      
-        fclose($fileLog);        
+        fwrite($fileLog, $json_string . "\n");
+        fclose($fileLog);
     }
 }
