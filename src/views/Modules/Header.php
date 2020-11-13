a5f6cf6f63427898608033b4d908484c3ffa10a3
renejfc <renejfc@gmail.com>
2020-11-13 12:16:00 +0100

    front almost finished

--- src/views/Modules/Header.php
+++ src/views/Modules/Header.php
@@ -1,3 +1,7 @@
-<header>
-  <h1 class="jumbotron text-center">Tickets</h1>
+<header class="u-flex u-center u-flex-row header header-fixed u-unselectable p-1 px-3">
+  <h1 class="header-brand">Tickets</h1>
+      <a class="btn btn-info animated fadeIn infinite alternate mx-2" href="?action=create">
+        <i class="fas fa-plus">
+        </i>
+      </a>
 </header>
